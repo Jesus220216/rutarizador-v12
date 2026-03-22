16da6b7c3f49d7e182516b3adf6595aa9392513b
Jesus220216 <jjr1602@gmail.com>
2026-03-22 01:26:24 -0400

    login.js

--- login.js
+++ login.js
@@ -1,43 +1,58 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { auth, db } from "./firebase.js";
 
 import {
-  getFirestore,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+
+import {
   doc,
   setDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-const db = getFirestore();
+// LOGIN
+window.login = async () => {
 
-registerBtn.addEventListener("click", async () => {
+  console.log("click login");
 
-  const email = loginEmail.value;
-  const password = loginPassword.value;
-  const refInput = referralCode.value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+// REGISTRO
+window.register = async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const ref = document.getElementById("ref").value;
 
   try {
 
-    // 🔐 crear usuario
     const userCred = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCred.user;
 
-    // 🎯 generar código único
-    const myCode = Math.random().toString(36).substring(2,8).toUpperCase();
+    const code = Math.random().toString(36).substring(2,8).toUpperCase();
 
-    // 💾 GUARDAR EN FIRESTORE (LO QUE TE FALTA)
-   await setDoc(doc(db, "users", user.uid), {
-  email: email,
-  referralCode: Math.random().toString(36).substring(2,8).toUpperCase(),
-  referredBy: refInput || null,
-  earnings: 0,
-  today: 0,
-  refs: 0,
-  createdAt: new Date()
-});
+    await setDoc(doc(db, "users", user.uid), {
+      email,
+      referralCode: code,
+      referredBy: ref || null,
+      earnings: 0,
+      today: 0,
+      refs: 0,
+      createdAt: new Date()
+    });
 
-    alert("Registrado correctamente");
+    alert("Cuenta creada");
 
-  } catch (error) {
-    alert(error.message);
+  } catch (e) {
+    alert(e.message);
   }
-
-});
+};
